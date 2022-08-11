64c007ddf7edcafe554a13c1d22cde278080e2f3
Tanjim605 <tanjimulislam2001@gmail.com>
2022-08-11 09:41:16 +0600

    second test

---
+++ Test_git.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define PI acos(-1.0)
+#define ll long long
+#define ull unsigned long long
+#define sp ' '
+#define vi vector<int>
+#define vll vector<long long>
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fora(cn) for(auto &x : (cn))
+#define tc    \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define CY cout<<"YES\n";
+#define CN cout<<"NO\n";
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int a,b;
+    cin >> a >> b;
+    cout << a+b;
+    
+    return 0;
+}
